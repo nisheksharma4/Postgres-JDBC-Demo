0f65b330664b0ea9a6927e3ec4021c84b00d1497
nisheksharma <nisheksharma@192.168.0.101>
2025-05-11 16:05:00 +0530

    Added working JDBC connection with PostgreSQL

--- src/com/jdbcProject/Main.java
+++ src/com/jdbcProject/Main.java
@@ -2,7 +2,10 @@ package com.jdbcProject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -16,6 +19,31 @@ public class Main {
         try(Connection conn = DriverManager.getConnection(url, user, password)){
         	
         	System.out.println("Connected to the postgreSql successfully!");
+        	
+        	// create table
+        	String createTableSQL = "CREATE TABLE IF NOT EXISTS users (" + "id SERIAL PRIMARY KEY, " + "name VARCHAR(100), " + "email VARCHAR(100) UNIQUE)";
+        	
+        	Statement stmt = conn.createStatement();
+            stmt.executeUpdate(createTableSQL);
+            System.out.println("Table 'users' created or already exists.");
+            
+         // Insert data
+            String insertSQL = "INSERT INTO users (name, email) VALUES (?, ?) ON CONFLICT DO NOTHING";
+            PreparedStatement pstmt = conn.prepareStatement(insertSQL);
+            pstmt.setString(1, "Nishek");
+            pstmt.setString(2, "Nishek@google.com");
+            pstmt.executeUpdate();
+            System.out.println("Data inserted.");
+            
+         // Read data
+            String selectSQL = "SELECT * FROM users";
+            ResultSet rs = stmt.executeQuery(selectSQL);
+            System.out.println("User Records:");
+            
+            while (rs.next()) {
+                System.out.printf("ID: %d, Name: %s, Email: %s%n", rs.getInt("id"), rs.getString("name"), rs.getString("email"));
+            }
+
         	
         } catch (SQLException e) {
 			// TODO Auto-generated catch block
